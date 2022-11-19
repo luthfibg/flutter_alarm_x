e4c4d9b07a46272db65858aa563b131c43acbbe6
luthfibg <luthfinewzoneonline@gmail.com>
2022-11-19 07:50:34 +0700

    [UPD] clock ui quick fill

--- lib/navpages/clock.dart
+++ lib/navpages/clock.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:nfp110/customs/clock_view.dart';
 
 class ClockPage extends StatefulWidget {
@@ -14,6 +15,10 @@ class _ClockPageState extends State<ClockPage> {
 
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedTime = DateFormat('HH:mm').format(now);
+    var formattedDate = DateFormat('EEE, d MMM').format(now);
+
     return Scaffold(
       backgroundColor: black87a,
       appBar: AppBar(
@@ -23,32 +28,39 @@ class _ClockPageState extends State<ClockPage> {
       body: Container(
         height: 500,
         width: 500,
-        padding: const EdgeInsets.all(32),
+        padding: const EdgeInsets.all(26),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Expanded(
-              child: Text(
-                'Clock',
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-            ),
-            const Expanded(
-              child: Text(
-                'Clock',
-                style: TextStyle(color: Colors.white, fontSize: 56),
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Column(
+                  children: [
+                    const Padding(
+                        padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0)),
+                    Text(
+                      formattedTime,
+                      style:
+                          const TextStyle(color: Colors.white, fontSize: 999),
+                    ),
+                  ],
+                ),
               ),
             ),
             Expanded(
-              child: Column(
-                children: const [
-                  Padding(padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 14.0)),
-                  Text(
-                    'Clock',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ],
+              child: FittedBox(
+                child: Column(
+                  children: [
+                    const Padding(
+                        padding: EdgeInsets.fromLTRB(0.0, 25.0, 0.0, 0.0)),
+                    Text(
+                      formattedDate,
+                      style: const TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                  ],
+                ),
               ),
             ),
             const ClockView(),
@@ -56,6 +68,23 @@ class _ClockPageState extends State<ClockPage> {
               child: Text(
                 'Time Zone',
                 style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.language,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Text(
+                    'UTC',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ],
               ),
             ),
           ],
